af1d636c503cda7d59fadcebb3e9e64db3dc8b01
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-04-27 18:06:40 +0700

    chore: update const var and config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+import { ENV_ERROR_MSG } from "./utils/constant";
 
 interface EnvType {
     key: string;
@@ -12,32 +11,47 @@ interface EnvType {
 
 const requiredEnv: Array<EnvType> = [
     {
-        key: 'TOKEN',
-        value: '',
+        key: "TOKEN",
+        value: "",
         action: process.env.DISCORD_TOKEN,
-        err_msg: 'Set up discord token first!',
+        err_msg: ENV_ERROR_MSG.TOKEN,
     },
     {
-        key: 'PUBLIC_KEY',
-        value: '',
+        key: "PUBLIC_KEY",
+        value: "",
         action: process.env.PUBLIC_KEY,
-        err_msg: 'Set up public key first!',
+        err_msg: ENV_ERROR_MSG.PUBLIC_KEY,
     },
     {
-        key: 'CLIENT_ID',
-        value: '',
+        key: "CLIENT_ID",
+        value: "",
         action: process.env.CLIENT_ID,
-        err_msg: 'Set up client id first!',
+        err_msg: ENV_ERROR_MSG.CLIENT_ID,
     },
     {
-        key: 'BE_DOMAIN',
-        value: '',
+        key: "BE_DOMAIN",
+        value: "",
         action: process.env.BE_DOMAIN,
-        err_msg: 'Set up BE domain first!',
+        err_msg: ENV_ERROR_MSG.BE_DOMAIN,
     },
 ];
 
 const loadEnv = (): Record<string, string> => {
+    const env_mode: string | undefined = process.env.NODE_ENV;
+    if (!env_mode) throw new Error(ENV_ERROR_MSG.NOT_CONFIGURED);
+
+    let env_name: string = "";
+
+    if (env_mode.toLowerCase() === "development") {
+        env_name = ".env.development";
+    } else if (env_mode.toLowerCase() === "porfuction") {
+        env_name = ".env.production";
+    } else {
+        throw new Error(ENV_ERROR_MSG.UNKNOWN_MODE_ENV);
+    }
+
+    dotenv.config({ path: path.resolve(__dirname, env_name) });
+
     const envConfig: Record<string, string> = {};
 
     for (const env of requiredEnv) {
@@ -53,4 +67,3 @@ const loadEnv = (): Record<string, string> => {
 };
 
 export const config = loadEnv();
-
---
+++ src/utils/constant.ts
@@ -0,0 +1,8 @@
+export const ENV_ERROR_MSG = {
+  NOT_CONFIGURED: "MODE_ENV is not configured!",
+  UNKNOWN_MODE_ENV: "Uknown mode env!",
+  TOKEN: "Set up discord token first!",
+  PUBLIC_KEY: "Set up public key first!",
+  CLIENT_ID: "Set up client id first!",
+  BE_DOMAIN: "Set up BE domain first!",
+};
