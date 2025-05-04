d814b21f667565be37d04ca39072296fef3aa11c
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-05-04 09:28:08 +0700

    ref: subscribe news and holidays cahnnel

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -9,4 +9,22 @@ export const ENV_ERROR_MSG = {
 
 export const CRON_TAB = {
     EVERYDAY_AT_1_PM: "0 13 * * *",
+    EVERY_FIRST_DATE_OF_MONTH_AT_8_AM: "0 8 1 * *",
+}
+
+export const CHANNEL = {
+    "news-channel": "",
+    "holiday-channel": "",
+}
+
+export const LIST_COMMANDS = {
+    GET_TODAY_NEWS: "get-today-news",
+    GET_PROFILE: "get-profile",
+    GET_TODAY_HOLIDAY: "get-today-holidays",
+    // Subscribe
+    SUBSCRIBE_NEWS: "subscribe-news",
+    SUBSCRIBE_HOLIDAY: "subscribe-holidays",
+    // Unsubscribe
+    UNSUBSCRIBE_NEWS: "unsubscribe-news",
+    UNSUBSCRIBE_HOLIDAY: "unsubscribe-holidays",
 }
